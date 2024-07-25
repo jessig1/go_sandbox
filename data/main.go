680346d2c88cbcb30a8a468fbfdbc3320dfbd082
Jakob Essig <essigj88@outlook.com>
2024-07-24 22:29:15 -0400

    maps

--- data/main.go
+++ data/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+// type alias makes it easier to define multiple types
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main() {
 	// dynamic array with initial length with max capacity of 5
 	userNames := make([]string, 2, 5)
@@ -12,11 +19,25 @@ func main() {
 
 	fmt.Println(userNames)
 
-	courseRatings := make(map[string]float64, 3)
+	courseRatings := make(floatMap, 3)
 
 	courseRatings["go"] = 4.7
 	courseRatings["Terraform"] = 4.6
 	courseRatings["Rust"] = 4.1
 
-	fmt.Println(courseRatings)
+	courseRatings.output()
+
+	//fmt.Println(courseRatings)
+
+	//iterates through array
+	for index, value := range userNames {
+		fmt.Print("index: ", index)
+		fmt.Println("value: ", value)
+	}
+
+	//iterate a map
+	for key, value := range courseRatings {
+		fmt.Println("key: ", key)
+		fmt.Println("value: ", value)
+	}
 }
